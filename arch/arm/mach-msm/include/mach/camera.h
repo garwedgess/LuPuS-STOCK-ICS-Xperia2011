495ebc78a5571acd7f3885cb412eb3301e0e2d9a
garwedgess <garethwilliams21@gmail.com>
2013-02-09 23:13:04 +0000

    mach-msm:support added phoenix

--- arch/arm/mach-msm/include/mach/camera.h
+++ arch/arm/mach-msm/include/mach/camera.h
@@ -180,7 +180,7 @@ struct msm_sensor_ctrl {
 	int (*s_init)(const struct msm_camera_sensor_info *);
 	int (*s_release)(void);
 	int (*s_config)(void __user *);
-#if defined(CONFIG_MACH_SEMC_ZEUS)
+#if defined(CONFIG_MACH_SEMC_ZEUS) || defined(CONFIG_MACH_SEMC_PHOENIX)
 	int (*s_get_capture_started)(void);
 #endif /* CONFIG_MACH_SEMC_ZEUS */
 };
@@ -274,7 +274,7 @@ struct msm_sync {
 	spinlock_t pmem_stats_spinlock;
 	spinlock_t abort_pict_lock;
 
-#if defined(CONFIG_MACH_SEMC_ZEUS)
+#if defined(CONFIG_MACH_SEMC_ZEUS) || defined(CONFIG_MACH_SEMC_PHOENIX)
 	uint8_t validframe;
 #endif /* CONFIG_MACH_SEMC_ZEUS */
 };
