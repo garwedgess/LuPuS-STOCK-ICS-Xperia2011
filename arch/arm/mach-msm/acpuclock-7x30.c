81ac900f21f3d5d2e191e411e1209362981a63e3
garwedgess <garethwilliams21@gmail.com>
2013-04-15 19:47:23 +0100

    mach-msm: updated frequency table

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -56,7 +56,6 @@
 #define MAX_AXI_KHZ 192000
 #define SEMC_ACPU_MIN_UV_MV 750U
 #define SEMC_ACPU_MAX_UV_MV 1525U
-#define OVERCLOCK_CPU_LOW 1 /* set to 0 to enable 2.0 GHz */
 
 struct clock_state {
 	struct clkctl_acpu_speed	*current_speed;
@@ -80,8 +79,11 @@ struct clkctl_acpu_speed {
 static struct clock_state drv_state = { 0 };
 
 static struct cpufreq_frequency_table freq_table[] = {
+	//{ 0, 24576 },
+	//{ 1, 61440 },
 	{ 0, 134400 },
 	{ 1, 184320 },
+	// 192000
 	{ 2, 249600 },
 	{ 3, 364800 },
 	{ 4, 460800 },
@@ -98,15 +100,7 @@ static struct cpufreq_frequency_table freq_table[] = {
 	{ 14, 1401600 },
 	{ 15, 1516800 },
 	{ 16, 1612800 },
-#if OVERCLOCK_CPU_LOW
 	{ 17, CPUFREQ_TABLE_END },
-#else
-	{ 17, 1708800 },
-	{ 18, 1804800 },
-	{ 19, 1900800 },
-	{ 20, 2016000 },
-	{ 21, CPUFREQ_TABLE_END },
-#endif
 };
 
 /* Use negative numbers for sources that can't be enabled/disabled */
@@ -119,8 +113,8 @@ static struct cpufreq_frequency_table freq_table[] = {
  * know all the h/w requirements.
  */
 static struct clkctl_acpu_speed acpu_freq_tbl[] = {
-//	{ 24576,  SRC_LPXO, 0, 0,  30720000,  900, VDD_RAW(900) },
-//	{ 61440,  PLL_3,    5, 11, 61440000,  900, VDD_RAW(900) },
+	//{ 24576,  SRC_LPXO, 0, 0,  30720000,  900, VDD_RAW(900) },
+	//{ 61440,  PLL_3,    5, 11, 61440000,  900, VDD_RAW(900) },
 	{ 134400, PLL_3,    5, 5,  61440000,  900, VDD_RAW(900) },
 	{ 184320, PLL_3,    5, 4,  61440000,  900, VDD_RAW(900) },
 	{ MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440000, 900, VDD_RAW(900) },
@@ -144,15 +138,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl[] = {
 	{ 1401600, PLL_2,   3, 0,  UINT_MAX, 1250, VDD_RAW(1250) },
 	{ 1516800, PLL_2,   3, 0,  UINT_MAX, 1300, VDD_RAW(1300) },
 	{ 1612800, PLL_2,   3, 0,  UINT_MAX, 1300, VDD_RAW(1300) },
-#if OVERCLOCK_CPU_LOW
 	{ 0 }
-#else
-	{ 1708800, PLL_2,   3, 0,  UINT_MAX, 1350, VDD_RAW(1350) },
-	{ 1804800, PLL_2,   3, 0,  UINT_MAX, 1400, VDD_RAW(1400) },
-	{ 1900800, PLL_2,   3, 0,  UINT_MAX, 1450, VDD_RAW(1450) },
-	{ 2016000, PLL_2,   3, 0,  UINT_MAX, 1500, VDD_RAW(1500) },
-	{ 0 }
-#endif
 };
 
 #define POWER_COLLAPSE_KHZ MAX_AXI_KHZ
