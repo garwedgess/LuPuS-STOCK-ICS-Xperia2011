803bfef1b6fad7c442459ac999cbf646c85a1933
garwedgess <garethwilliams21@gmail.com>
2013-06-11 11:27:57 +0100

    Revert "block:ROW IO Scheduler Backported from CAF 3.4"
    This reverts commit eb06a90f5633f78dc17ac745bf33f2b414f3358f.

--- include/linux/elevator.h
+++ include/linux/elevator.h
@@ -17,10 +17,7 @@ typedef int (elevator_allow_merge_fn) (struct request_queue *, struct request *,
 typedef int (elevator_dispatch_fn) (struct request_queue *, int);
 
 typedef void (elevator_add_req_fn) (struct request_queue *, struct request *);
-typedef int (elevator_reinsert_req_fn) (struct request_queue *,
-					struct request *);
 typedef int (elevator_queue_empty_fn) (struct request_queue *);
-typedef bool (elevator_is_urgent_fn) (struct request_queue *);
 typedef struct request *(elevator_request_list_fn) (struct request_queue *, struct request *);
 typedef void (elevator_completed_req_fn) (struct request_queue *, struct request *);
 typedef int (elevator_may_queue_fn) (struct request_queue *, int);
@@ -37,13 +34,11 @@ struct elevator_ops
 {
 	elevator_merge_fn *elevator_merge_fn;
 	elevator_merged_fn *elevator_merged_fn;
-	elevator_is_urgent_fn *elevator_is_urgent_fn;
 	elevator_merge_req_fn *elevator_merge_req_fn;
 	elevator_allow_merge_fn *elevator_allow_merge_fn;
 
 	elevator_dispatch_fn *elevator_dispatch_fn;
 	elevator_add_req_fn *elevator_add_req_fn;
-	elevator_reinsert_req_fn *elevator_reinsert_req_fn;
 	elevator_activate_req_fn *elevator_activate_req_fn;
 	elevator_deactivate_req_fn *elevator_deactivate_req_fn;
 
@@ -109,7 +104,6 @@ extern void elv_merge_requests(struct request_queue *, struct request *,
 			       struct request *);
 extern void elv_merged_request(struct request_queue *, struct request *, int);
 extern void elv_requeue_request(struct request_queue *, struct request *);
-extern int elv_reinsert_request(struct request_queue *, struct request *);
 extern int elv_queue_empty(struct request_queue *);
 extern struct request *elv_former_request(struct request_queue *, struct request *);
 extern struct request *elv_latter_request(struct request_queue *, struct request *);
